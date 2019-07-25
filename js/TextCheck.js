4e713dec7704736f5698f24f37951fda2e56bee3
sarasaranuranura <wada121400@gmail.com>
2019-07-25 13:50:19 +0900

    フリガナ,住所,メールアドレスのそれぞれの入力チェックが完成

--- js/TextCheck.js
+++ js/TextCheck.js
@@ -1,17 +1,63 @@
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function () {
 	var name = document.getElementById("name");//名前入力欄
-	
+	var kana = document.getElementById("kana");//フリガナ入力欄
+	var address = document.getElementById("address");//住所入力欄
+	var email = document.getElementById("email");//メールアドレス入力欄
+
 	/**
 	 * 名前入力をリアルタイムにチェックする
 	 */
-	name.addEventListener("keyup", function(){
+	name.addEventListener("keyup", function () {
 		var text = name.value;
 
-		console.log(text +" "+text.length);			
+		console.log("name:" + text + " " + text.length);
 		if (text) {
 			var NameRequiredMessage = document.getElementById("NameRequiredMessage");
 			NameRequiredMessage.textContent = "名前入力完了";
 			NameRequiredMessage.style.color = "blue";
 		}
-	},false);
-},false);
+	}, false);
+
+	/**
+	 * フリガナ入力をリアルタイムにチェックする
+	 */
+	kana.addEventListener("keyup", function () {
+		var text = kana.value;
+
+		console.log("kana:" + text + " " + text.length);
+		if (text) {
+			var HuriganaRequiredMessage = document.getElementById("HuriganaRequiredMessage");
+			HuriganaRequiredMessage.textContent = "フリガナ入力完了";
+			HuriganaRequiredMessage.style.color = "blue";
+		}
+	}, false);
+
+	/**
+	 * 住所入力をリアルタイムにチェックする
+	 */
+	address.addEventListener("keyup", function () {
+		var text = address.value;
+
+		console.log("address:" + text + " " + text.length);
+		if (text) {
+			var StreetAddressRequiredMessage = document.getElementById("StreetAddressRequiredMessage");
+			StreetAddressRequiredMessage.textContent = "住所入力完了";
+			StreetAddressRequiredMessage.style.color = "blue";
+		}
+	}, false);
+
+	/**
+	 * メールアドレス入力をリアルタイムにチェックする
+	 */
+	email.addEventListener("keyup", function () {
+		var text = email.value;
+
+		console.log("email:" + text + " " + text.length);
+		if (text) {
+			var　EmailRequiredMessage = document.getElementById("EmailRequiredMessage");
+			EmailRequiredMessage.textContent = "メールアドレス入力完了";
+			EmailRequiredMessage.style.color = "blue";
+		}
+	}, false);
+
+}, false);
