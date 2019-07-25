3a2922c365e4649ba55ed0b78b61bd9107cd30c9
sarasaranuranura <wada121400@gmail.com>
2019-07-25 14:04:57 +0900

    パスワードの入力チェックを10文字以上にした

--- js/TextCheck.js
+++ js/TextCheck.js
@@ -3,18 +3,22 @@ document.addEventListener("DOMContentLoaded", function () {
 	var kana = document.getElementById("kana");//フリガナ入力欄
 	var address = document.getElementById("address");//住所入力欄
 	var email = document.getElementById("email");//メールアドレス入力欄
+	var password = document.getElementById("password");//パスワード入力欄
 
 	/**
 	 * 名前入力をリアルタイムにチェックする
 	 */
 	name.addEventListener("keyup", function () {
 		var text = name.value;
-
 		console.log("name:" + text + " " + text.length);
+		
+		var NameRequiredMessage = document.getElementById("NameRequiredMessage");
 		if (text) {
-			var NameRequiredMessage = document.getElementById("NameRequiredMessage");
 			NameRequiredMessage.textContent = "名前入力完了";
 			NameRequiredMessage.style.color = "blue";
+		}else {
+			NameRequiredMessage.textContent = "*必須";
+			NameRequiredMessage.style.color = "red";
 		}
 	}, false);
 
@@ -23,12 +27,15 @@ document.addEventListener("DOMContentLoaded", function () {
 	 */
 	kana.addEventListener("keyup", function () {
 		var text = kana.value;
-
 		console.log("kana:" + text + " " + text.length);
+		
+		var HuriganaRequiredMessage = document.getElementById("HuriganaRequiredMessage");
 		if (text) {
-			var HuriganaRequiredMessage = document.getElementById("HuriganaRequiredMessage");
 			HuriganaRequiredMessage.textContent = "フリガナ入力完了";
 			HuriganaRequiredMessage.style.color = "blue";
+		}else {
+			HuriganaRequiredMessage.textContent = "*必須";
+			HuriganaRequiredMessage.style.color = "red";
 		}
 	}, false);
 
@@ -37,12 +44,15 @@ document.addEventListener("DOMContentLoaded", function () {
 	 */
 	address.addEventListener("keyup", function () {
 		var text = address.value;
-
 		console.log("address:" + text + " " + text.length);
+		
+		var StreetAddressRequiredMessage = document.getElementById("StreetAddressRequiredMessage");
 		if (text) {
-			var StreetAddressRequiredMessage = document.getElementById("StreetAddressRequiredMessage");
 			StreetAddressRequiredMessage.textContent = "住所入力完了";
 			StreetAddressRequiredMessage.style.color = "blue";
+		}else {
+			StreetAddressRequiredMessage.textContent = "*必須";
+			StreetAddressRequiredMessage.style.color = "red";
 		}
 	}, false);
 
@@ -51,13 +61,33 @@ document.addEventListener("DOMContentLoaded", function () {
 	 */
 	email.addEventListener("keyup", function () {
 		var text = email.value;
-
 		console.log("email:" + text + " " + text.length);
+		
+		var　EmailRequiredMessage = document.getElementById("EmailRequiredMessage");
 		if (text) {
-			var　EmailRequiredMessage = document.getElementById("EmailRequiredMessage");
 			EmailRequiredMessage.textContent = "メールアドレス入力完了";
 			EmailRequiredMessage.style.color = "blue";
+		}else {
+			EmailRequiredMessage.textContent = "*必須";
+			EmailRequiredMessage.style.color = "red";
 		}
 	}, false);
 
+	/**
+	 * パスワード入力をリアルタイムにチェックする(7文字以上じゃないといけない)
+	 */
+	password.addEventListener("keyup", function () {
+		var MinPassWordLength = 10;
+		var text = password.value;
+		console.log("password:" + text + " " + text.length);
+		
+		var　PassWordRequiredMessage = document.getElementById("PassWordRequiredMessage");
+		if (text.length >= MinPassWordLength) {
+			PassWordRequiredMessage.textContent = "パスワード入力完了";
+			PassWordRequiredMessage.style.color = "blue";
+		}else {
+			PassWordRequiredMessage.textContent = "*あと"+ (MinPassWordLength - text.length) +"入力してください";
+			PassWordRequiredMessage.style.color = "red";
+		}
+	}, false);
 }, false);
